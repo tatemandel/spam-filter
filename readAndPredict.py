db5e0845c85d5867720383b866285fac2f608606
Tate Mandel <tmandel@seas.upenn.edu>
2013-12-19 16:52:30 -0500

    [TM] Started setting up checking for spam/ham

--- readAndPredict.py
+++ readAndPredict.py
@@ -6,6 +6,8 @@ import glob
 import pickle
 
 if __name__ == '__main__':
+    spam = 0
+    ham = 0
     if len(sys.argv) < 3:
         print 'Usage:', sys.argv[0], 'pickleFile, testFileOrFolder'
     else:
@@ -15,7 +17,14 @@ if __name__ == '__main__':
             if os.path.isdir(sys.argv[2]):
                 # predict all files in folder
                 for f in glob.glob(sys.argv[2]+'/*'):
-                    print f, 'is', predictor.predict(f)
+                    prediction = predictor.predict(f)
+                    print f, 'is', prediction
+                    if prediction:
+                        spam = spam + 1
+                    else:
+                        ham = ham + 1
+                print("spam:", str(spam))
+                print("ham:", str(ham))
             elif os.path.isfile(sys.argv[2]):
                 # predict this file
                 print sys.argv[2], 'is', predictor.predict(sys.argv[2])
